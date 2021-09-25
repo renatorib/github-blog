611ef9cfef34497efc2be31b758c494bec730caa
Renato <renatoribroman@gmail.com>
2021-09-25 19:43:36 -0300

    use token as env var

--- e2e/getLabels.test.ts
+++ e2e/getLabels.test.ts
@@ -2,7 +2,7 @@ import { GithubBlog } from "../dist/github-blog";
 
 const blog = new GithubBlog({
   repo: "renatorib/github-blog-tests",
-  token: process.env.GITHUB_E2E_TESTS_TOKEN!,
+  token: process.env.E2E_TESTS_TOKEN!,
 });
 
 describe("getLabels", () => {
